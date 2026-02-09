fa487589c50d6774dc73dd2427d2d57888283f19
jamesnathan <jamesnathanaguilar1621@gmail.com>
2026-02-09 18:33:40 +0800

    Added row and column

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,39 @@
 <template>
   <div>
+    
    <h1>Welcome to My Nuxt  App BOYYYYYY</h1> 
-   <v-btn color="red"> Submit</v-btn>
+   
+   <v-row>
+
+    <v-col cols="12" md="3">
+    <v-card color="red" height="120px">
+      <v-card-text>
+        <v-row>
+          <v-col cols="6">
+            <v-card-title class="text-h4">
+              <v-icon size="100">
+                mdi-home   
+              </v-icon>
+            </v-card-title>
+          </v-col>
+         <v-col cols="6">
+          <v-card-title>
+            <v-row no-gutters>
+              <v-col cols="12" class="text-h6 text-right">Amount</v-col>
+              <v-col cols="12" class="text-h4 text-right front-italic"> $0987</v-col>
+            </v-row>
+          </v-card-title>
+         </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+   </v-col>
+  
+  </v-row> 
   </div>
+
+
 </template>
 
-<script lang="ts" setup>
-
-</script>
-
-<style>
-
-</style>
+<script lang="ts" setup></script>
+<style ></style> 
